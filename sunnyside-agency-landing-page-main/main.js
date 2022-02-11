3ffcd7872c6d7ca7f9df8ac34f30eec8a7838629
Mustafa Gündoğdu <48795429+msxtafa@users.noreply.github.com>
2022-02-11 16:45:33 +0100

    Add files via upload

---
+++ sunnyside-agency-landing-page-main/main.js
@@ -0,0 +1,8 @@
+let mobileNav = document.getElementById("mobile-nav");
+let hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", function () {
+
+    mobileNav.classList.toggle("d-block");
+
+}, false)
