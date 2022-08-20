ace7ef82a2881db8901bce9a5f72a7ad612b911a
Mustafa Gündoğdu <48795429+msxtafa@users.noreply.github.com>
2022-08-20 13:46:01 +0200

    Add files via upload

---
+++ url-shortening-api-master/main.js
@@ -0,0 +1,12 @@
+
+
+
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".mobile-navigation");
+
+hamburger.addEventListener("click", function(){
+
+nav.classList.toggle("d-none");
+
+})
+
